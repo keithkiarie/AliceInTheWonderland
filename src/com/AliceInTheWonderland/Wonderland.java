535dceaf142dcbdfec856d2c60fa397d729e08ee
Keithkiarie <keithkiarie@gmail.com>
2020-08-28 20:55:33 +0300

    java project configuration

---
+++ src/com/AliceInTheWonderland/Wonderland.java
@@ -0,0 +1,9 @@
+package com.AliceInTheWonderland;
+
+public class Wonderland {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Welcome to Wonderland!");
+    }
+}
