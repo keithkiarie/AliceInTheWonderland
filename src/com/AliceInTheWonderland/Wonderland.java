724140fb90adc6662f0af53a1237ad468d889e89
Keithkiarie <keithkiarie@gmail.com>
2020-08-29 12:53:08 +0300

    a gamesession is an instance of Wonderland

--- src/com/AliceInTheWonderland/Wonderland.java
+++ src/com/AliceInTheWonderland/Wonderland.java
@@ -1,15 +1,38 @@
 package com.AliceInTheWonderland;
 
+import java.util.HashMap;
+
 public class Wonderland {
 
+    HashMap<String, Location> Locations;
+
     public static void main(String[] args) {
-	    PostWelcomeMessage();
-	    Location LongHall = new Location("Locations/LongHall.json");
+        Wonderland GameSession = new Wonderland();
     }
 
-    static void PostWelcomeMessage() {
+    Wonderland() {
+        PostWelcomeMessage();
+        ConstructLocations();
+    }
+
+    void PostWelcomeMessage() {
         System.out.println("\n");
         System.out.println("Welcome to Wonderland!");
         System.out.println("\n");
+    }
+
+    void ConstructLocations() {
+
+        Locations = new HashMap<String, Location>();
+
+        Locations.put("LongHall", new Location("Locations/LongHall.json"));
+        Locations.put("Garden", new Location("Locations/Garden.json"));
+        Locations.put("Courtroom", new Location("Locations/Courtroom.json"));
+        Locations.put("MarchHaresHouse", new Location("Locations/MarchHaresHouse.json"));
+        Locations.put("DuchessHouse", new Location("Locations/DuchessHouse.json"));
+        Locations.put("CroquetPlayground", new Location("Locations/CroquetPlayground.json"));
+        Locations.put("RabbitsHouse", new Location("Locations/RabbitsHouse.json"));
+        Locations.put("Shores", new Location("Locations/Shores.json"));
+        Locations.put("Central", new Location("Locations/Central.json"));
     }
 }
