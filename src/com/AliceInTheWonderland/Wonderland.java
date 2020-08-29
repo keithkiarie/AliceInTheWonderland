0b68d0609179af62c8d9697d9a83f0c262764491
Keithkiarie <keithkiarie@gmail.com>
2020-08-29 17:58:23 +0300

    variables that describe game state

--- src/com/AliceInTheWonderland/Wonderland.java
+++ src/com/AliceInTheWonderland/Wonderland.java
@@ -2,9 +2,22 @@ package com.AliceInTheWonderland;
 
 import java.util.HashMap;
 
+// Plot is influenced by who is met first between the Gardeners and the Rabbit, determining who will help you
+enum Plot {
+    Duchess, Rabbit
+}
+
 public class Wonderland {
 
-    HashMap<String, Location> Locations;
+    public static Plot GamePlot;
+    public static boolean CriedAtLongHall = false;
+    public static boolean PaidRabbit = false;
+    public static boolean TimeIsTakenBack = false;
+
+
+    public static Location CurrentLocation;
+
+    public static int Plot = 1;
 
     public static void main(String[] args) {
         Wonderland GameSession = new Wonderland();
@@ -23,16 +36,15 @@ public class Wonderland {
 
     void ConstructLocations() {
 
-        Locations = new HashMap<String, Location>();
-
-        Locations.put("LongHall", new Location("Locations/LongHall.json"));
-        Locations.put("Garden", new Location("Locations/Garden.json"));
-        Locations.put("Courtroom", new Location("Locations/Courtroom.json"));
-        Locations.put("MarchHaresHouse", new Location("Locations/MarchHaresHouse.json"));
-        Locations.put("DuchessHouse", new Location("Locations/DuchessHouse.json"));
-        Locations.put("CroquetPlayground", new Location("Locations/CroquetPlayground.json"));
-        Locations.put("RabbitsHouse", new Location("Locations/RabbitsHouse.json"));
-        Locations.put("Shores", new Location("Locations/Shores.json"));
-        Locations.put("SafeRoom", new Location("Locations/SafeRoom.json"));
+        Location.DeepWell = new Location("Locations/DeepWell.json");
+        Location.LongHall = new Location("Locations/LongHall.json");
+        Location.Garden = new Location("Locations/Garden.json");
+        Location.Courtroom = new Location("Locations/Courtroom.json");
+        Location.MarchHaresHouse = new Location("Locations/MarchHaresHouse.json");
+        Location.DuchessHouse = new Location("Locations/DuchessHouse.json");
+        Location.CroquetPlayground = new Location("Locations/CroquetPlayground.json");
+        Location.RabbitsHouse = new Location("Locations/RabbitsHouse.json");
+        Location.Shores = new Location("Locations/Shores.json");
+        Location.SafeRoom = new Location("Locations/SafeRoom.json");
     }
 }
